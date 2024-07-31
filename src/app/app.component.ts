8b264c9af9a417612cd3d295df1ca8bb26a4f597
ajmathis1231 <ajmathis1231@gmail.com>
2024-07-30 21:22:11 -0500

    Banks created, interface and details working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BanksComponent } from "./banks/banks.component";
+import { BankDetailComponent } from './bank-detail/bank-detail.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', /* This selector identifies this component as the root, the "app" prefix in all these fields, ID's it as such as well. */
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BanksComponent, BankDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'banks';
-}
+export class AppComponent { 
+  title = 'Investment Banks';
+ }
---
+++ src/app/bank.ts
@@ -0,0 +1,7 @@
+export interface Bank {
+    id: number;
+    name: string;
+    website: string;
+}
+// All new key,value pairs need to be added here in the interface ts file, in the banks.ts file (which is the object list), and lastly in the html file.
+
